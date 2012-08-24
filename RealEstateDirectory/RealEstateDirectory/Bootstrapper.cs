f1ac8c97c221641b1d79b3da8844b83bf51bbf32
Maxim Semenov <Maxim Semenov@localhost>
2012-08-24 10:02:25 +0400

    Implement base solution.

---
+++ RealEstateDirectory/RealEstateDirectory/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using RealEstateDirectory.Shell;
+
+namespace RealEstateDirectory
+{
+	public class Bootstrapper : UnityBootstrapper
+	{
+		protected override DependencyObject CreateShell()
+		{
+			return Container.Resolve<ShellView>();
+		}
+
+		protected override void InitializeShell()
+		{
+			base.InitializeShell();
+			((ShellView) Shell).DataContext = Container.Resolve<ShellViewModel>();
+			Application.Current.MainWindow = (Window) Shell;
+		}
+
+		public override void Run(bool runWithDefaultConfiguration)
+		{
+			base.Run(runWithDefaultConfiguration);
+			Application.Current.MainWindow.Show();
+		}
+	}
+}
---
+++ RealEstateDirectory/RealEstateDirectory/Shell/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using Microsoft.Practices.ServiceLocation;
+
+namespace RealEstateDirectory.Shell
+{
+	public class ShellViewModel : NotificationObject
+	{
+		public ShellViewModel(IServiceLocator serviceLocator)
+		{
+			_ServiceLocator = serviceLocator;
+		}
+
+		#region Infrastructure
+
+		private readonly IServiceLocator _ServiceLocator;
+
+		#endregion
+	}
+}
