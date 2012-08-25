b77ecf6893b84e2ab864d48e50b471093b3dba70
Maxim Semenov <Maxim Semenov@localhost>
2012-08-25 22:53:46 +0400

    ???????? ????? ??? ???????? ??????? ???????? ??.

---
+++ RealEstateDirectory/RealEstateDirectory/Shell/ShellViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.Prism.ViewModel;
+using Microsoft.Practices.ServiceLocation;
+using NotifyPropertyWeaver;
+
+namespace RealEstateDirectory.Shell
+{
+	[NotifyForAll]
+	public class ShellViewModel : NotificationObject
+	{
+		public ShellViewModel(IServiceLocator serviceLocator)
+		{
+			_ServiceLocator = serviceLocator;
+		}
+
+		#region Infrastructure
+
+		private readonly IServiceLocator _ServiceLocator;
+
+		#endregion
+	}
+}
