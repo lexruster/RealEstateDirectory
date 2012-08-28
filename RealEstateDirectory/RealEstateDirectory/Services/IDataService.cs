31a628deedfd3b0618a6993152bc79fcb787edd8
Maxim Semenov <Maxim Semenov@localhost>
2012-08-28 14:37:49 +0400

    ????????? ????-???????.

---
+++ RealEstateDirectory/RealEstateDirectory/Services/IDataService.cs
@@ -0,0 +1,25 @@
+﻿namespace RealEstateDirectory.Services
+{
+	public interface IDataService
+	{
+		void CreateArea(string name);
+		void UpdateArea(int id, string name);
+		void DeleteArea(int id);
+
+		void CreateLayout(string name);
+		void UpdateLayout(int id, string name);
+		void DeleteLayout(int id);
+
+		void CreateSewage(string name);
+		void UpdateSewage(int id, string name);
+		void DeleteSewage(int id);
+
+		void CreateStreet(string name);
+		void UpdateStreet(int id, string name);
+		void DeleteStreet(int id);
+
+		void CreateState(string name);
+		void UpdateState(int id, string name);
+		void DeleteState(int id);
+	}
+}
