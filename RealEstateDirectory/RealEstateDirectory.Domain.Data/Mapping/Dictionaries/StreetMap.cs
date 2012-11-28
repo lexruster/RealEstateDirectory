447c114b6bab7ff4816eb9280254028b7816e0ce
Maxim Semenov <Maxim Semenov@localhost>
2012-11-28 11:13:09 +0400

    Backed out changeset: b912d524f412

--- RealEstateDirectory/RealEstateDirectory.Domain.Data/Mapping/Dictionaries/StreetMap.cs
+++ RealEstateDirectory/RealEstateDirectory.Domain.Data/Mapping/Dictionaries/StreetMap.cs
@@ -4,28 +4,25 @@ using RealEstateDirectory.Domain.Entities.Dictionaries;
 
 namespace RealEstateDirectory.Domain.Data.Mapping.Dictionaries
 {
-	public class StreetMap : ClassMapping<Street>
-	{
-		public StreetMap()
-		{
-			Id(x => x.Id, m => m.Generator(Generators.Identity));
-
-			ManyToOne(x => x.District, r =>
-				{
-					r.Cascade(Cascade.All | Cascade.None | Cascade.Persist | Cascade.Remove);
-					r.Update(true);
-					r.Insert(true);
-					r.NotNullable(true);
-					r.Class(typeof (District));
-					r.Column("DistrictId");
-				});
-
-			Property(x => x.Name, m =>
-				{
-					m.NotNullable(true);
-					m.Length(2048);
-					m.Unique(true);
-				});
-		}
-	}
+    public class StreetMap : ClassMapping<Street>
+    {
+        public StreetMap()
+        {
+            Id(x => x.Id, m => m.Generator(Generators.Identity));
+            Property(x => x.Name, m =>
+            {
+                m.NotNullable(true);
+                m.Length(2048);
+                m.Unique(true);
+            });
+            ManyToOne(x => x.District, r =>
+                                           {
+                                               r.Cascade(Cascade.All | Cascade.None | Cascade.Persist | Cascade.Remove);
+                                               r.Update(true);
+                                               r.Insert(true);
+                                               r.NotNullable(true);
+                                               r.Class(typeof(District));
+                                           });
+        }
+    }
 }
--- RealEstateDirectory/RealEstateDirectory.Domain.Data/Mapping/RealEstateMap.cs
+++ RealEstateDirectory/RealEstateDirectory.Domain.Data/Mapping/RealEstateMap.cs
@@ -4,25 +4,25 @@ using RealEstateDirectory.Domain.Entities;
 
 namespace RealEstateDirectory.Domain.Data.Mapping
 {
-	public class RealEstateMap : ClassMapping<RealEstate>
-	{
-		public RealEstateMap()
-		{
-			Id(x => x.Id, m => m.Generator(Generators.Identity));
+    public class RealEstateMap : ClassMapping<RealEstate>
+    {
+        public RealEstateMap()
+        {
+            Id(x => x.Id, m => m.Generator(Generators.Identity));
+            ManyToOne(x => x.DealVariant);
+            Property(x => x.Description, m => m.Length(4096));
 
-			ManyToOne(x => x.DealVariant, mapper => mapper.Column("DealVariantId"));
-			ManyToOne(x => x.District, mapper => mapper.Column("DistrictId"));
-			ManyToOne(x => x.Realtor, mapper => mapper.Column("RealtorId"));
-			ManyToOne(x => x.Street, mapper => mapper.Column("StreetId"));
-			ManyToOne(x => x.Ownership, mapper => mapper.Column("OwnershipId"));
+            ManyToOne(x => x.District);
+            Property(x => x.HasVideo);
+            Property(x => x.Price);
 
-			Property(x => x.Description, m => m.Length(4096));
-			Property(x => x.HasVideo);
-			Property(x => x.Price);
-			Property(x => x.SubmitToDomino);
-			Property(x => x.SubmitToVDV);
-			Property(x => x.TerritorialNumber);
-			Property(x => x.CreateDate);
-		}
-	}
+            ManyToOne(x => x.Realtor);
+            ManyToOne(x => x.Street);
+            Property(x => x.SubmitToDomino);
+            Property(x => x.SubmitToVDV);
+            Property(x => x.TerritorialNumber);
+            Property(x => x.CreateDate);
+            ManyToOne(x => x.Ownership);
+        }
+    }
 }
