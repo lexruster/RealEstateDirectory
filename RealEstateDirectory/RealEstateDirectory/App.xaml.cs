437be8d42b5f9d70a3c38adc21a6cbff29a8ffa1
Maxim Semenov <Maxim Semenov@localhost>
2012-12-04 17:24:50 +0400

    ????????? ???????? ??? ??????.

--- RealEstateDirectory/RealEstateDirectory/App.xaml.cs
+++ RealEstateDirectory/RealEstateDirectory/App.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using RealEstateDirectory.Migrations;
 using RealEstateDirectory.Misc;
 
 namespace RealEstateDirectory
@@ -16,20 +20,37 @@ namespace RealEstateDirectory
 		{
 			base.OnStartup(e);
 
+			LoadConfig();
+			RunMigrations();
+
+			var bootstrapper = new Bootstrapper();
+			bootstrapper.Run();
+		}
+
+		private static void LoadConfig()
+		{
 			Utils.Config.Load();
 			if (String.IsNullOrEmpty(Utils.Config.GetProperty("DefaultConnectionString")))
 			{
 				var configWindow = new ConfigWindow();
 				var result = configWindow.ShowDialog();
 				if (!result.HasValue || !result.Value)
-				{
-					Application.Current.Shutdown();
-					return;
-				}
+					Current.Shutdown();
 			}
+		}
 
-            var bootstrapper = new Bootstrapper();
-			bootstrapper.Run();
+		private void RunMigrations()
+		{
+			var context = new RunnerContext(new NullAnnouncer())
+				{
+					Database = "postgresql",
+					Connection = Utils.Config.GetProperty("DefaultConnectionString"),
+					Target = Assembly.GetAssembly(typeof (MigrationsBeacon)).Location,
+					PreviewOnly = false,
+					NestedNamespaces = false,
+					Task = "migrate"
+				};
+			new TaskExecutor(context).Execute();
 		}
 	}
 }
