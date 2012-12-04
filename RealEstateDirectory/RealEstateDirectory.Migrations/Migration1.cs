523ea7ba6ab98edbf1fd0dd1b37669f3772b0e66
Maxim Semenov <Maxim Semenov@localhost>
2012-12-04 16:44:00 +0400

    ?????????? ?????????? ???? Decimal ? ?????????. ???????? Runner.

--- RealEstateDirectory/RealEstateDirectory.Migrations/Migration1.cs
+++ RealEstateDirectory/RealEstateDirectory.Migrations/Migration1.cs
@@ -5,10 +5,10 @@ using FluentMigrator;
 namespace RealEstateDirectory.Migrations
 {
 	[Migration(1)]
-    public class Migration1 : Migration
-    {
-	    public override void Up()
-	    {
+	public class Migration1 : Migration
+	{
+		public override void Up()
+		{
 			CreateDictionaryTable("DealVariant");
 			CreateDictionaryTable("District");
 			CreateDictionaryTable("FloorLevel");
@@ -23,63 +23,63 @@ namespace RealEstateDirectory.Migrations
 			CreateDictionaryTable("ToiletType");
 			CreateDictionaryTable("WaterSupply");
 
-		    Create.Table("RealEstate")
-			    .WithColumn("Id").AsInt32().PrimaryKey().Identity().NotNullable()
-			    .WithColumn("TerritorialNumber").AsString().Nullable()
-			    .WithColumn("Description").AsString(Int32.MaxValue).Nullable()
-			    .WithColumn("HasVideo").AsBoolean().NotNullable()
-			    .WithColumn("SubmitToVDV").AsBoolean().NotNullable()
-			    .WithColumn("SubmitToDomino").AsBoolean().NotNullable()
-			    .WithColumn("Price").AsDecimal(19, 5).Nullable()
-			    .WithColumn("CreateDate").AsDateTime().NotNullable()
-			    .WithColumn("DistrictId").AsInt32().Nullable().ForeignKey("FK_RealEstate_District", "District", "Id")
-			    .WithColumn("StreetId").AsInt32().Nullable().ForeignKey("FK_RealEstate_Street", "Street", "Id")
-			    .WithColumn("RealtorId").AsInt32().Nullable().ForeignKey("FK_RealEstate_Realtor", "Realtor", "Id")
-			    .WithColumn("DealVariantId").AsInt32().Nullable().ForeignKey("FK_RealEstate_DealVariant", "DealVariant", "Id")
-			    .WithColumn("OwnershipId").AsInt32().Nullable().ForeignKey("FK_RealEstate_Ownership", "DealVariant", "Id");
+			Create.Table("RealEstate")
+				.WithColumn("Id").AsInt32().PrimaryKey().Identity().NotNullable()
+				.WithColumn("TerritorialNumber").AsString().Nullable()
+				.WithColumn("Description").AsString(Int32.MaxValue).Nullable()
+				.WithColumn("HasVideo").AsBoolean().NotNullable()
+				.WithColumn("SubmitToVDV").AsBoolean().NotNullable()
+				.WithColumn("SubmitToDomino").AsBoolean().NotNullable()
+				.WithColumn("Price").AsDecimal(5, 19).Nullable()
+				.WithColumn("CreateDate").AsDateTime().NotNullable()
+				.WithColumn("DistrictId").AsInt32().Nullable().ForeignKey("FK_RealEstate_District", "District", "Id")
+				.WithColumn("StreetId").AsInt32().Nullable().ForeignKey("FK_RealEstate_Street", "Street", "Id")
+				.WithColumn("RealtorId").AsInt32().Nullable().ForeignKey("FK_RealEstate_Realtor", "Realtor", "Id")
+				.WithColumn("DealVariantId").AsInt32().Nullable().ForeignKey("FK_RealEstate_DealVariant", "DealVariant", "Id")
+				.WithColumn("OwnershipId").AsInt32().Nullable().ForeignKey("FK_RealEstate_Ownership", "DealVariant", "Id");
 
-		    Create.Table("Building")
-			    .WithColumn("RealEstateId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Building_RealEstate", "RealEstate", "Id").OnDelete(Rule.Cascade)
-			    .WithColumn("Floor").AsInt32().Nullable()
-			    .WithColumn("TotalFloor").AsInt32().Nullable()
-			    .WithColumn("TotalSquare").AsDecimal(19,5).Nullable()
-			    .WithColumn("MaterialId").AsInt32().Nullable().ForeignKey("FK_Building_Material", "Material", "Id");
+			Create.Table("Building")
+				.WithColumn("RealEstateId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Building_RealEstate", "RealEstate", "Id").OnDelete(Rule.Cascade)
+				.WithColumn("Floor").AsInt32().Nullable()
+				.WithColumn("TotalFloor").AsInt32().Nullable()
+				.WithColumn("TotalSquare").AsDecimal(5, 19).Nullable()
+				.WithColumn("MaterialId").AsInt32().Nullable().ForeignKey("FK_Building_Material", "Material", "Id");
 
-		    Create.Table("Apartment")
-			    .WithColumn("BuildingId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Apartment_Building", "Building", "RealEstateId").OnDelete(Rule.Cascade)
-			    .WithColumn("TotalRoomCount").AsInt32().Nullable()
-			    .WithColumn("LayoutId").AsInt32().Nullable().ForeignKey("FK_Apartment_Layout", "Layout", "Id")
-			    .WithColumn("TerraceId").AsInt32().Nullable().ForeignKey("FK_Apartment_Terrace", "Terrace", "Id")
-			    .WithColumn("FloorLevelId").AsInt32().Nullable().ForeignKey("FK_Apartment_FloorLevel", "FloorLevel", "Id");
+			Create.Table("Apartment")
+				.WithColumn("BuildingId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Apartment_Building", "Building", "RealEstateId").OnDelete(Rule.Cascade)
+				.WithColumn("TotalRoomCount").AsInt32().Nullable()
+				.WithColumn("LayoutId").AsInt32().Nullable().ForeignKey("FK_Apartment_Layout", "Layout", "Id")
+				.WithColumn("TerraceId").AsInt32().Nullable().ForeignKey("FK_Apartment_Terrace", "Terrace", "Id")
+				.WithColumn("FloorLevelId").AsInt32().Nullable().ForeignKey("FK_Apartment_FloorLevel", "FloorLevel", "Id");
 
-		    Create.Table("Flat")
+			Create.Table("Flat")
 				.WithColumn("ApartmentId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Flat_Apartment", "Apartment", "BuildingId").OnDelete(Rule.Cascade)
-			    .WithColumn("ResidentialSquare").AsDecimal(19,5).Nullable()
-			    .WithColumn("KitchenSquare").AsDecimal(19,5).Nullable()
+				.WithColumn("ResidentialSquare").AsDecimal(5, 19).Nullable()
+				.WithColumn("KitchenSquare").AsDecimal(5, 19).Nullable()
 				.WithColumn("ToiletTypeId").AsInt32().Nullable().ForeignKey("FK_Flat_ToiletType", "ToiletType", "Id");
 
-		    Create.Table("Plot")
-			    .WithColumn("RealEstateId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Plot_RealEstate", "RealEstate", "Id").OnDelete(Rule.Cascade)
-			    .WithColumn("PlotSquare").AsDecimal(19, 5).Nullable();
+			Create.Table("Plot")
+				.WithColumn("RealEstateId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Plot_RealEstate", "RealEstate", "Id").OnDelete(Rule.Cascade)
+				.WithColumn("PlotSquare").AsDecimal(5, 19).Nullable();
 
-		    Create.Table("House")
+			Create.Table("House")
 				.WithColumn("PlotId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_House_Plot", "Plot", "RealEstateId").OnDelete(Rule.Cascade)
-			    .WithColumn("TotalFloor").AsInt32().Nullable()
-			    .WithColumn("HouseSquare").AsDecimal(19, 5).Nullable()
-			    .WithColumn("HasBathhouse").AsBoolean().Nullable()
-			    .WithColumn("HasGarage").AsBoolean().Nullable()
-			    .WithColumn("HasGas").AsBoolean().Nullable()
-			    .WithColumn("WaterSupplyId").AsInt32().Nullable().ForeignKey("FK_House_WaterSupply", "WaterSupply", "Id")
-			    .WithColumn("SewageId").AsInt32().Nullable().ForeignKey("FK_House_Sewage", "Sewage", "Id")
-			    .WithColumn("MaterialId").AsInt32().Nullable().ForeignKey("FK_House_Material", "Material", "Id");
+				.WithColumn("TotalFloor").AsInt32().Nullable()
+				.WithColumn("HouseSquare").AsDecimal(5, 19).Nullable()
+				.WithColumn("HasBathhouse").AsBoolean().Nullable()
+				.WithColumn("HasGarage").AsBoolean().Nullable()
+				.WithColumn("HasGas").AsBoolean().Nullable()
+				.WithColumn("WaterSupplyId").AsInt32().Nullable().ForeignKey("FK_House_WaterSupply", "WaterSupply", "Id")
+				.WithColumn("SewageId").AsInt32().Nullable().ForeignKey("FK_House_Sewage", "Sewage", "Id")
+				.WithColumn("MaterialId").AsInt32().Nullable().ForeignKey("FK_House_Material", "Material", "Id");
 
-		    Create.Table("Residence")
+			Create.Table("Residence")
 				.WithColumn("BuildingId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Residence_Building", "Building", "RealEstateId").OnDelete(Rule.Cascade);
 
-		    Create.Table("Room")
+			Create.Table("Room")
 				.WithColumn("ApartmentId").AsInt32().PrimaryKey().NotNullable().ForeignKey("FK_Room_Apartment", "Apartment", "BuildingId").OnDelete(Rule.Cascade)
-			    .WithColumn("RoomCount").AsInt32().Nullable();
-	    }
+				.WithColumn("RoomCount").AsInt32().Nullable();
+		}
 
 		private void CreateDictionaryTable(string tableName)
 		{
@@ -88,9 +88,9 @@ namespace RealEstateDirectory.Migrations
 				.WithColumn("Name").AsString(2000).Unique("IX_" + tableName + "_Name").NotNullable();
 		}
 
-	    public override void Down()
-	    {
-		    Delete.Table("Room");
+		public override void Down()
+		{
+			Delete.Table("Room");
 			Delete.Table("Residence");
 			Delete.Table("House");
 			Delete.Table("Plot");
@@ -113,5 +113,5 @@ namespace RealEstateDirectory.Migrations
 			Delete.Table("ToiletType");
 			Delete.Table("WaterSupply");
 		}
-    }
+	}
 }
