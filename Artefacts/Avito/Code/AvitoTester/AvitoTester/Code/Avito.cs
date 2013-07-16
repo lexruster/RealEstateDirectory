b9fa5a336640bb49649e28ca5d3c1a5d164f480d
Alexander <lexruster@gmail.com>
2013-07-16 18:25:05 +0400

    ???????????? ? ???? ???????????? ??? ?????

---
+++ Artefacts/Avito/Code/AvitoTester/AvitoTester/Code/Avito.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace AvitoTester.Code
+{
+    public class Avito
+    {
+
+        public void Run()
+        {
+
+            var ads = new Ads();
+
+            ads.formatVersion = 1;
+            ads.target = "Avito.ru";
+
+            ads.Ad = new AdsAD[3];
+
+            ads.Ad[0] = GetAd(1);
+            ads.Ad[1] = GetAd(2);
+            ads.Ad[2] = GetAd(3);
+
+            //Serialize the class into a file
+            XmlSerializer xs = new XmlSerializer(typeof (Ads));
+            FileStream fs = new FileStream(@"D:\Test\test.xml", FileMode.Create);
+            xs.Serialize(fs, ads);
+            fs.Close();
+
+
+
+
+            //Deserialize the file into an instance of the class
+            xs = new XmlSerializer(typeof (Ads));
+            fs = new FileStream(@"D:\Test\test.xml", FileMode.Open);
+            Ads mc = (Ads) xs.Deserialize(fs);
+            fs.Close();
+
+        }
+
+        private AdsAD GetAd(int i)
+        {
+            var ad = new AdsAD
+                {
+                    AdStatus = "Free",
+                    Category = "Комнаты",
+                    City = "Волгоград",
+                    ContactPhone = "+7 984 541 84 45",
+                    Description = "Лучшая комната " + i,
+                    Floor = 1,
+                    Floors = 2,
+                    HouseType = "Кирпичный",
+                    Id = "1_" + i,
+                    OperationType = "Продам",
+                    SaleRooms = 2,
+                    Rooms = 3,
+                    Square = 14,
+
+                };
+
+            return ad;
+        }
+    }
+}
---
+++ Artefacts/Avito/Code/AvitoTester/AvitoTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvitoTester.Code;
+
+namespace AvitoTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var av = new Avito();
+
+            av.Run();
+        }
+    }
+}
